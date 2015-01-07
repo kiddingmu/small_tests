5016c57e97b5d40ed4d392397267c049ab02b5b8
kiddingmu <shunanfei@gmail.com>
2015-01-07 15:07:16 +0800

    Maintain fraction in lowest terms during the construction

--- 04_class_example/fraction.py
+++ 04_class_example/fraction.py
@@ -3,6 +3,9 @@ class Fraction:
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
+        common = gcd(self.num, self.den)
+        self.num /= common
+        self.den /= common
 
     def __str__(self):
         return str(self.num) + "/" + str(self.den)
@@ -10,10 +13,15 @@ class Fraction:
     def show(self):
         print self.num, "/", self.den
 
+    def getNum(self):
+        return self.num
+
+    def getDen(self):
+        return self.den
+
     def __add__(self, otherfraction):
         newnum = self.num * otherfraction.den + self.den * otherfraction.num
         newden = self.den * otherfraction.den
-        common = gcd(newnum, newden)
         return Fraction(newnum, newden)
 
     def __eq__(self, other):
@@ -25,19 +33,16 @@ class Fraction:
     def __mul__(self, otherfraction):
         newnum = self.num * otherfraction.num
         newden = self.den * otherfraction.den
-        common = gcd(newnum, newden)
         return Fraction(newnum, newden)
 
     def __div__(self, otherfraction):
         newnum = self.num * otherfraction.den
         newden = self.den * otherfraction.num
-        common = gcd(newnum, newden)
         return Fraction(newnum, newden)
 
     def __sub__(self, otherfraction):
         newnum = self.num * otherfraction.den - self.den * otherfraction.num
         newden = self.den * otherfraction.den
-        common = gcd(newnum, newden)
         return Fraction(newnum, newden)
 
     def __le__(self, otherfraction):
@@ -59,7 +64,7 @@ def gcd(m, n):
     return n
 
 if __name__ == "__main__":
-    x = Fraction(1,2)
+    x = Fraction(5,6)
     y = Fraction(2,3)
     print x + y
     print x == y
