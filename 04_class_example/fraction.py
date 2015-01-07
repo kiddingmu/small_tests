e0284bf94c84df7e15f4e0dbfa903dfa9c2486df
kiddingmu <shunanfei@gmail.com>
2015-01-07 17:15:43 +0800

    Add more operation for the class Fraction

--- 04_class_example/fraction.py
+++ 04_class_example/fraction.py
@@ -1,6 +1,8 @@
 class Fraction:
 
     def __init__(self, top, bottom):
+        if not isinstance(top, int) or not isinstance(bottom, int):
+            raise TypeError("The data type of numerator or denominator is not int")
         self.num = top
         self.den = bottom
         common = gcd(self.num, self.den)
@@ -8,6 +10,9 @@ class Fraction:
         self.den /= common
 
     def __str__(self):
+        return str(self.num) + "/" + str(self.den)
+
+    def __repr__(self):
         return str(self.num) + "/" + str(self.den)
 
     def show(self):
@@ -40,16 +45,36 @@ class Fraction:
         newden = self.den * otherfraction.num
         return Fraction(newnum, newden)
 
+    def __truediv__(self, otherfraction):
+        newnum = self.num * otherfraction.den
+        newden = self.den * otherfraction.num
+        return Fraction(newnum, newden)
+
+    def __floordiv__(self, otherfraction):
+        newnum = self.num * otherfraction.den
+        newden = self.den * otherfraction.num
+        #return Fraction(newnum, newden)
+        return newnum // newden
+
     def __sub__(self, otherfraction):
         newnum = self.num * otherfraction.den - self.den * otherfraction.num
         newden = self.den * otherfraction.den
         return Fraction(newnum, newden)
 
-    def __le__(self, otherfraction):
+    def __lt__(self, otherfraction):
         return self.num * otherfraction.den < self.den * otherfraction.num
 
     def __gt__(self, otherfraction):
         return self.num * otherfraction.den > self.den * otherfraction.num
+
+    def __le__(self, otherfraction):
+        return self.num * otherfraction.den <= self.den * otherfraction.num
+
+    def __ge__(self, otherfraction):
+        return self.num * otherfraction.den >= self.den * otherfraction.num
+
+    def __ne__(self, otherfraction):
+        return self.num * otherfraction.den != self.den * otherfraction.num
 
 
 def gcd(m, n):
@@ -70,6 +95,8 @@ if __name__ == "__main__":
     print x == y
     print x * y
     print x / y
+    print x // y
     print x - y
     print x < y
     print x > y
+    z = Fraction(1.0, 2)
